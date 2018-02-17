4fb29d34b02ab68f1a19bdb64da019b03ce9cd02
Varaprasadh <varaprasadh.a@gmail.com>
2018-02-17 15:53:59 +0530

    more on interface
    @lambda exxpressions.//static method defination in interface
    @functional interface

---
+++ Static_M_Interface.java
@@ -0,0 +1,26 @@
+
+/**
+ * @author vara
+ * from java 1.8 its possible to define static methods in interface;
+ * and also if interface has only one method called as functional interface 
+ * then it can be implemented by lambda expressions
+ * 
+ * 
+ */
+interface StaticInterface{
+    void show();
+    static void display(){
+        System.out.println("static method \"display\"");
+    }
+}
+
+
+public class Static_M_Interface {
+    public static void main(String[] as){
+     StaticInterface si=()->{
+       System.out.println("implementation of show with lambda expressions");
+    };
+        
+      StaticInterface.display();   
+    }
+}
